8e28b5e0aff4eb598d85a58fa7449c6bf809122b
uS-aito <h2so40627@gmail.com>
2020-04-12 22:51:35 +0900

    条件が間違ってたりコマンドが不完全だったりを修正

--- create_GKE_cluster.sh
+++ create_GKE_cluster.sh
@@ -2,12 +2,17 @@
 
 CLUSTER_NAME_PREFIX="gke-cluster-"
 CLUSTER_CREATE_COMMAND="gcloud container clusters create ${CLUSTER_NAME_PREFIX}$1 --zone=us-central1-c --num-nodes 3"
-CLUSTER_EXIST_CHECK_COMMAND="gcloud container clusters describe ${CLUSTER_NAME_PREFIX}$1"
+CLUSTER_EXIST_CHECK_COMMAND="gcloud container clusters describe ${CLUSTER_NAME_PREFIX}$1 --zone=us-central1-c"
 
+bold() {
+  echo "$(tput bold)""$*" "$(tput sgr0)";
+}
+
+bold ". Checking cluster ${CLUSTER_NAME_PREFIX}$1 exist..."
 $CLUSTER_EXIST_CHECK_COMMAND
 if [ $? -eq 0 ]; then
-    $CLUSTER_CREATE_COMMAND
-else
-    echo "Cluster ${CLUSTER_NAME_PREFIX}$1 already exist."
+    bold "Cluster ${CLUSTER_NAME_PREFIX}$1 already exist."
     exit 0
+else
+    $CLUSTER_CREATE_COMMAND
 fi
