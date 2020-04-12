1eef72e3c9499f4c1b407e06f29bba0ca5bcb0e4
uS-aito <h2so40627@gmail.com>
2020-04-12 23:04:02 +0900

    メッセージを修正

--- create_GKE_cluster.sh
+++ create_GKE_cluster.sh
@@ -11,8 +11,9 @@ bold() {
 bold ". Checking cluster ${CLUSTER_NAME_PREFIX}$1 exist..."
 $CLUSTER_EXIST_CHECK_COMMAND 2>&1 > /dev/null
 if [ $? -eq 0 ]; then
-    bold "Cluster ${CLUSTER_NAME_PREFIX}$1 already exist."
+    bold ". Cluster ${CLUSTER_NAME_PREFIX}$1 already exist."
     exit 0
 else
+    bold ". Cluster ${CLUSTER_NAME_PREFIX}$1 does not exist. Start deploy..."
     $CLUSTER_CREATE_COMMAND
 fi
